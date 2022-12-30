5e27e99d54d4fdb7fbea8bff0b9457e312abbf9e
Hakimi keerai <hakeemee222@gmail.com>
2022-12-30 22:48:58 +0700

    test

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request,render_template
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('index.html')
 
 @app.route('/success/<name>')
 def success(name):
+   # เช็คเงื่อนไขให้ Name ที่กรอกมาตรงตามที่กำหนด
    if name == 'Hakeemee':
       return '<h2>welcome %s</h2>' % name
    else: 
       return '<h2>sorry %s, login is failed</h2>' % name
 
-@app.route('/login',methods = ['POST', 'GET'])
+
+# Route ไปยัง form เพื่อที่จะรับค่า Post ที่ได้มา
+
+@app.route('/login',methods = ['POST', 'GET']) 
 def login():
    if request.method == 'POST':
       user = request.form['nm']
-      return redirect(url_for('success',name = user))
+      return redirect(url_for('success',name = user)) 
+      # เรียกใช้ฟังก์ชั่น success เเละส่ง parameter 'nm' ไปยังฟังก์ชัน
    else:
       user = request.args.get('nm')
       return redirect(url_for('success',name = user))
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(host='0.0.0.0',port='5000',debug=True)
